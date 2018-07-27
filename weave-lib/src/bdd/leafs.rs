a5ddeb3b848a77c5b7337f5b8f5022cc3df88304
theochupp <tclchiam@gmail.com>
2018-07-27 12:44:13 -0400

    Renamed the Node enums to actually make sense: Leaves are terminating, branches lead to more nodes

--- weave-lib/src/bdd/leafs.rs
+++ weave-lib/src/bdd/leafs.rs
@@ -1,30 +1,31 @@
 use closet::Item;
-use std::collections::HashMap;
 
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
 pub enum Node<'a> {
-    Terminal(bool),
-    Leaf(&'a Item, Box<Node<'a>>, Box<Node<'a>>),
+    TrueLeaf,
+    FalseLeaf,
+    Branch(&'a Item, Box<Node<'a>>, Box<Node<'a>>),
 }
 
 impl<'a> Node<'a> {
-    fn leaf(id: &'a Item, left: Node<'a>, right: Node<'a>) -> Node<'a> {
-        Node::Leaf(id, Box::new(left), Box::new(right))
+    fn branch(id: &'a Item, left: Node<'a>, right: Node<'a>) -> Node<'a> {
+        Node::Branch(id, Box::new(left), Box::new(right))
     }
 }
 
 fn reduce<'a>(node: &'a Node<'a>) -> Node<'a> {
     return match node {
-        Node::Terminal(_val) => node.clone(),
-        Node::Leaf(item, ref l, ref r) => {
-            let l = reduce(l);
-            let r = reduce(r);
+        Node::TrueLeaf => node.clone(),
+        Node::FalseLeaf => node.clone(),
+        Node::Branch(item, ref left, ref right) => {
+            let reduced_left = reduce(left);
+            let reduced_right = reduce(right);
 
-            if l == r {
-                return l;
+            if reduced_left == reduced_right {
+                return reduced_left;
             }
 
-            return Node::leaf(item, l, r);
+            return Node::branch(item, reduced_left, reduced_right);
         }
     };
 }
@@ -39,19 +40,19 @@ mod tests {
 
     #[test]
     fn or_can_be_reduced_if_left_and_right_are_equal() {
-        let false_node = Terminal(false);
-        let true_node = Terminal(true);
+        let false_node = FalseLeaf;
+        let true_node = TrueLeaf;
 
         let jeans = Item::new("jeans");
         let blue_shirt = Item::new("blue_shirt");
 
-        let left_leaf = Node::leaf(&jeans, false_node.clone(), true_node.clone());
-        let right_leaf = Node::leaf(&jeans, false_node.clone(), true_node.clone());
-        let parent_leaf = Node::leaf(&blue_shirt, left_leaf, right_leaf);
+        let left_branch = Node::branch(&jeans, false_node.clone(), true_node.clone());
+        let right_branch = Node::branch(&jeans, false_node.clone(), true_node.clone());
+        let parent_branch = Node::branch(&blue_shirt, left_branch, right_branch);
 
-        let actual = reduce(&parent_leaf);
+        let actual = reduce(&parent_branch);
 
-        let expected = Node::leaf(&jeans, false_node, true_node);
+        let expected = Node::branch(&jeans, false_node, true_node);
         assert_eq!(
             expected,
             actual
@@ -60,19 +61,19 @@ mod tests {
 
     #[test]
     fn sibling_relationship_cannot_be_reduced_in_nodes() {
-        let false_node = Terminal(false);
-        let true_node = Terminal(true);
+        let false_node = FalseLeaf;
+        let true_node = TrueLeaf;
 
         let jeans = Item::new("jeans");
         let blue_shirt = Item::new("blue_shirt");
 
-        let left_leaf = Node::leaf(&jeans, true_node.clone(), false_node.clone());
-        let right_leaf = Node::leaf(&jeans, false_node.clone(), true_node.clone());
-        let parent_leaf = Node::leaf(&blue_shirt, left_leaf.clone(), right_leaf.clone());
+        let left_branch = Node::branch(&jeans, true_node.clone(), false_node.clone());
+        let right_branch = Node::branch(&jeans, false_node.clone(), true_node.clone());
+        let parent_branch = Node::branch(&blue_shirt, left_branch.clone(), right_branch.clone());
 
-        let actual = reduce(&parent_leaf);
+        let actual = reduce(&parent_branch);
 
-        let expected = Node::leaf(&blue_shirt, left_leaf, right_leaf);
+        let expected = Node::branch(&blue_shirt, left_branch, right_branch);
         assert_eq!(
             expected,
             actual
@@ -81,19 +82,19 @@ mod tests {
 
     #[test]
     fn exclusion_rule_can_be_reduced() {
-        let false_node = Terminal(false);
-        let true_node = Terminal(true);
+        let false_node = FalseLeaf;
+        let true_node = TrueLeaf;
 
         let jeans = Item::new("jeans");
         let blue_shirt = Item::new("blue_shirt");
 
-        let left_leaf = Node::leaf(&jeans, true_node.clone(), true_node.clone());
-        let right_leaf = Node::leaf(&jeans, true_node.clone(), false_node.clone());
-        let parent_leaf = Node::leaf(&blue_shirt, left_leaf.clone(), right_leaf.clone());
+        let left_branch = Node::branch(&jeans, true_node.clone(), true_node.clone());
+        let right_branch = Node::branch(&jeans, true_node.clone(), false_node.clone());
+        let parent_branch = Node::branch(&blue_shirt, left_branch.clone(), right_branch.clone());
 
-        let actual = reduce(&parent_leaf);
+        let actual = reduce(&parent_branch);
 
-        let expected = Node::leaf(&blue_shirt, true_node.clone(), right_leaf);
+        let expected = Node::branch(&blue_shirt, true_node.clone(), right_branch);
         assert_eq!(
             expected,
             actual
@@ -102,19 +103,19 @@ mod tests {
 
     #[test]
     fn inclusion_rule_can_be_reduced() {
-        let false_node = Terminal(false);
-        let true_node = Terminal(true);
+        let false_node = FalseLeaf;
+        let true_node = TrueLeaf;
 
         let jeans = Item::new("jeans");
         let blue_shirt = Item::new("blue_shirt");
 
-        let left_leaf = Node::leaf(&jeans, true_node.clone(), true_node.clone());
-        let right_leaf = Node::leaf(&jeans, false_node.clone(), true_node.clone());
-        let parent_leaf = Node::leaf(&blue_shirt, left_leaf.clone(), right_leaf.clone());
+        let left_branch = Node::branch(&jeans, true_node.clone(), true_node.clone());
+        let right_branch = Node::branch(&jeans, false_node.clone(), true_node.clone());
+        let parent_branch = Node::branch(&blue_shirt, left_branch.clone(), right_branch.clone());
 
-        let actual = reduce(&parent_leaf);
+        let actual = reduce(&parent_branch);
 
-        let expected = Node::leaf(&blue_shirt, true_node.clone(), right_leaf);
+        let expected = Node::branch(&blue_shirt, true_node.clone(), right_branch);
         assert_eq!(
             expected,
             actual
