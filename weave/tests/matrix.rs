3bf5a7134ffc8c36d58c3d4a039ede2abb542dd2
theochupp <tclchiam@gmail.com>
2019-03-06 14:06:07 -0500

    Wrapped the Forest tests in a module so its easy to see what failed

--- weave/tests/matrix.rs
+++ weave/tests/matrix.rs
@@ -3,10 +3,13 @@ extern crate weave;
 #[macro_use]
 mod forest;
 
-intersect_tests!(weave::matrix::Forest<&str>);
+#[cfg(test)]
+mod matrix_tests {
+    intersect_tests!(weave::matrix::Forest<&str>);
 
-union_tests!(weave::matrix::Forest<&str>);
+    union_tests!(weave::matrix::Forest<&str>);
 
-product_tests!(weave::matrix::Forest<&str>);
+    product_tests!(weave::matrix::Forest<&str>);
 
-subset_tests!(weave::matrix::Forest<&str>);
+    subset_tests!(weave::matrix::Forest<&str>);
+}
--- weave/tests/zdd.rs
+++ weave/tests/zdd.rs
@@ -4,13 +4,16 @@ extern crate weave;
 #[macro_use]
 mod forest;
 
-intersect_tests!(weave::zdd2::Forest<&str>);
+#[cfg(test)]
+mod zdd_tests {
+    intersect_tests!(weave::zdd2::Forest<&str>);
 
-union_tests!(weave::zdd2::Forest<&str>);
+    union_tests!(weave::zdd2::Forest<&str>);
 
-product_tests!(weave::zdd2::Forest<&str>);
+    product_tests!(weave::zdd2::Forest<&str>);
 
-subset_tests!(weave::zdd2::Forest<&str>);
+    subset_tests!(weave::zdd2::Forest<&str>);
+}
 
 #[cfg(test)]
 mod serde_tests {
